ec263cf7b1e8abf2e6594b0f2415a1e56e526de1
Bruno Gregório <bgregorioribeiro@gmail.com>
2024-06-27 22:01:46 -0300

    resolvendo conflitos

--- api/db/dbConnection.js
+++ api/db/dbConnection.js
@@ -6,7 +6,7 @@ const sqlConnection = mysql.createConnection({
   host: "localhost",       
   port: 3306,              
   user: "root",           
-  password: "admin",       
+  password: "",       
   database: "agenda",    
 });
 
